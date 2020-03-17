b67f8ec927322a4d78607045e8fbf2d32e226d5f
Gordon Soutter <gordon.soutter@outlook.com>
2020-03-17 16:58:49 +0000

    Initial commit. Files created and planning notes done

---
+++ specs/bear_spec.rb
@@ -0,0 +1,15 @@
+require("minitest/autorun")
+require('minitest/reporters')
+require_relative('../river')
+require_relative('../bear')
+
+
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+
+
+class CustomerTest < MiniTest::Test
+
+
+
+
+end
